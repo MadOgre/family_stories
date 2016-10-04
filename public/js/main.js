e70e14988461fe979e03430081e3337e9a2d4767
Mike Semko <msemko@gmail.com>
2016-10-03 22:25:48 -0400

    Revert "moved tab-pane styles into own class 'tab-content'"
    This reverts commit 96756c79e74be2fa56ba51dbc6e915174e8da9dc.

--- public/js/main.js
+++ public/js/main.js
@@ -4,7 +4,6 @@
 
   function Main($http, $scope, $location, sharedProperties) {
     var vm = this;
-    vm.currentTab = "0";
     vm.results = [];
     function updateCurrentAvatar() {
       vm.currentAvatar.images = vm.results[vm.currentAvatarIndex-1].images.slice();
