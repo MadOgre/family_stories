89ab0f4b6454bc5f98965dddc6ca79544d029ced
Mike Semko <msemko@gmail.com>
2016-11-05 20:52:04 -0400

    Integrated new views, changed angular routing

--- public/js/routes.js
+++ public/js/routes.js
@@ -8,15 +8,30 @@
         controller: "Main",
         controllerAs: "main"
       })
-      .when("/getpreview", {
-        templateUrl: "/partials/preview.html",
-        controller: "Preview",
-        controllerAs: "preview"
+      .when("/previewfamily", {
+        templateUrl: "/partials/pfpreview_1.html",
+        controller: "Main",
+        controllerAs: "main"
+      })
+      .when("/previewbook", {
+        templateUrl: "/partials/pfpreview_2.html",
+        controller: "Main",
+        controllerAs: "main"
+      })
+      .when("/dedication", {
+        templateUrl: "/partials/pfpreview_3.html",
+        controller: "Main",
+        controllerAs: "main"
       })
       .when("/oldmain", {
         templateUrl: "/partials/old_main.html",
         controller: "Main",
         controllerAs: "main"
+      })
+      .when("/getpreview", {
+        templateUrl: "/partials/preview.html",
+        controller: "Preview",
+        controllerAs: "preview"        
       })
       .otherwise({
         redirectTo: '/'
